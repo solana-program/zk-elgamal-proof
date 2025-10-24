84cbec70f555476391f7d36198789ac268b11da9
Sam Kim <skim13@cs.stanford.edu>
2025-10-24 10:56:51 +0900

    add a `test-browser` feature to run unit tests in both node and browser

--- zk-sdk-wasm-js/src/lib.rs
+++ zk-sdk-wasm-js/src/lib.rs
@@ -4,6 +4,16 @@
 pub mod encryption;
 pub mod proof_data;
 
+// Conditional Test Configuration
+// This block is only active when running tests (cfg(test)) on Wasm architecture.
+#[cfg(all(test, target_arch = "wasm32"))]
+mod wasm_test_config {
+    use wasm_bindgen_test::*;
+
+    #[cfg(feature = "test-browser")]
+    wasm_bindgen_test_configure!(run_in_browser);
+}
+
 /// Simple macro for implementing conversion functions between wrapper types and
 /// wrapped types.
 mod conversion {
