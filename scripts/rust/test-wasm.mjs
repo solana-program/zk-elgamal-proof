201adefe2decd454ae88076039a4749303d0f64a
Sam Kim <skim13@cs.stanford.edu>
2025-10-26 13:17:58 +0900

    add headless test for chrome in the test-wasm script

--- scripts/rust/test-wasm.mjs
+++ scripts/rust/test-wasm.mjs
@@ -7,3 +7,4 @@ const cratePath = path.join(workingDirectory, folder);
 
 await $`wasm-pack test --node ${cratePath} ${args}`;
 await $`wasm-pack test --headless --firefox ${cratePath} ${args} --features test-browser`;
+await $`wasm-pack test --headless --chrome ${cratePath} ${args} --features test-browser`;
