bd82cc7a9ce22be040d6ee46344dcd0ee7a64aa4
samkim-crypto <skim13@cs.stanford.edu>
2025-12-02 15:49:03 +0900

    bump eslint version

---
+++ clients/js/eslint.config.mjs
@@ -0,0 +1,22 @@
+import eslint from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['src/generated/**'],
+  },
+  eslint.configs.recommended,
+  tseslint.configs.eslintRecommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  prettier,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+);
