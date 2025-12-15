7b6f23b64358c1ae458686278141e9fa0c6ff5a1
samkim-crypto <skim13@cs.stanford.edu>
2025-12-15 12:45:30 +0400

    add a doc comment on the `generic` module

--- clients/js/src/generic/index.ts
+++ clients/js/src/generic/index.ts
@@ -1,3 +1,8 @@
+/**
+ * This module is handcrafted but intentionally structured to
+ * mirror the output of a client generated via the Codama library.
+ */
+
 export * from './accounts';
 export * from './errors';
 export * from './instructions';
