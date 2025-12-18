eb12394c0b0c757f0c751e747d8b7cb86a796c99
Michal R <vad.sol@proton.me>
2025-12-18 18:12:35 +0100

    zk-sdk: Do not require passing owned vectors to `RangeProof`
    The current implementation of `RangeProof::new` and `RangeProof::verify`
    requires the caller to pass inputs as owned vectors. That requires heap
    allocations even if the number of inputs is small (even as small as 1).
    
    Remove that requirement by accepting `AsRef<[T]>` trait as inputs, which
    still makes it possible to pass `Vec<T>`, so we don't break the old
    users relying on the old API. But for callers who want performance, it
    accepts also the following types that can be allocated on the stack:
    
    - `[T]`
    - `[T; N]`
    - `&[T]`
    
    Furthermore, single elements can be passed using `slice::from_ref`[0],
    e.g.
    
    ```rust
    let proof = RangeProof::new(
        slice::from_ref(&55),
        slice::from_ref(&64),
        slice::from_ref(&open),
        &mut transcript_create,
    )
    ```
    
    which does not perform any allocation at all. Instead, it performs raw
    pointer casting to coerce a reference to a single-element slice[1][2].
    
    Additionally, `AsRef<T>` makes it possible to pass the following
    iterable types:
    
    - `std::slice::Iter<'_, T>`
    - `IterMut<'_, T>`
    - `IntoIter<T, A>`
    
    [0] https://doc.rust-lang.org/std/slice/fn.from_ref.html
    [1] https://github.com/rust-lang/rust/blob/1.92.0/library/core/src/slice/raw.rs#L203-L205
    [2] https://github.com/rust-lang/rust/blob/1.92.0/library/core/src/array/mod.rs#L164-L167

--- zk-sdk/src/zk_elgamal_proof_program/batched_range_proof/batched_range_proof_u64.rs
+++ zk-sdk/src/zk_elgamal_proof_program/batched_range_proof/batched_range_proof_u64.rs
@@ -12,17 +12,26 @@ use {
         },
     },
     solana_zk_elgamal_proof_interface::proof_data::BatchedRangeProofU64Data,
-    std::convert::TryInto,
+    std::{borrow::Borrow, convert::TryInto},
 };
 
-pub fn build_batched_range_proof_u64_data(
-    commitments: Vec<&PedersenCommitment>,
-    amounts: Vec<u64>,
-    bit_lengths: Vec<usize>,
-    openings: Vec<&PedersenOpening>,
-) -> Result<BatchedRangeProofU64Data, ProofGenerationError> {
+pub fn build_batched_range_proof_u64_data<C, PC, A, B, O, PO>(
+    commitments: C,
+    amounts: A,
+    bit_lengths: B,
+    openings: O,
+) -> Result<BatchedRangeProofU64Data, ProofGenerationError>
+where
+    C: AsRef<[PC]>,
+    PC: Borrow<PedersenCommitment>,
+    A: AsRef<[u64]>,
+    B: AsRef<[usize]>,
+    O: AsRef<[PO]>,
+    PO: Borrow<PedersenOpening>,
+{
     // the sum of the bit lengths must be 64
     let batched_bit_length = bit_lengths
+        .as_ref()
         .iter()
         .try_fold(0_usize, |acc, &x| acc.checked_add(x))
         .ok_or(ProofGenerationError::IllegalAmountBitLength)?;
@@ -36,7 +45,7 @@ pub fn build_batched_range_proof_u64_data(
     }
 
     let context =
-        build_batched_range_proof_context(&commitments, &amounts, &bit_lengths, &openings)?;
+        build_batched_range_proof_context(commitments, &amounts, &bit_lengths, &openings)?;
 
     let mut transcript = batched_range_proof_transcript(&context);
     let proof = RangeProof::new(amounts, bit_lengths, openings, &mut transcript)?
@@ -69,7 +78,7 @@ impl VerifyZkProof for BatchedRangeProofU64Data {
         let proof: RangeProof = self.proof.try_into()?;
 
         proof
-            .verify(commitments.iter().collect(), bit_lengths, &mut transcript)
+            .verify(commitments, bit_lengths, &mut transcript)
             .map_err(|e| e.into())
     }
 }
@@ -105,7 +114,7 @@ mod test {
         let (commitment_8, opening_8) = Pedersen::new(amount_8);
 
         let proof_data = build_batched_range_proof_u64_data(
-            vec![
+            &[
                 &commitment_1,
                 &commitment_2,
                 &commitment_3,
@@ -115,11 +124,11 @@ mod test {
                 &commitment_7,
                 &commitment_8,
             ],
-            vec![
+            &[
                 amount_1, amount_2, amount_3, amount_4, amount_5, amount_6, amount_7, amount_8,
             ],
-            vec![8, 8, 8, 8, 8, 8, 8, 8],
-            vec![
+            &[8, 8, 8, 8, 8, 8, 8, 8],
+            &[
                 &opening_1, &opening_2, &opening_3, &opening_4, &opening_5, &opening_6, &opening_7,
                 &opening_8,
             ],
@@ -147,7 +156,7 @@ mod test {
         let (commitment_8, opening_8) = Pedersen::new(amount_8);
 
         let proof_data = build_batched_range_proof_u64_data(
-            vec![
+            &[
                 &commitment_1,
                 &commitment_2,
                 &commitment_3,
@@ -157,11 +166,11 @@ mod test {
                 &commitment_7,
                 &commitment_8,
             ],
-            vec![
+            &[
                 amount_1, amount_2, amount_3, amount_4, amount_5, amount_6, amount_7, amount_8,
             ],
-            vec![8, 8, 8, 8, 8, 8, 8, 8],
-            vec![
+            &[8, 8, 8, 8, 8, 8, 8, 8],
+            &[
                 &opening_1, &opening_2, &opening_3, &opening_4, &opening_5, &opening_6, &opening_7,
                 &opening_8,
             ],
--- zk-sdk/src/zk_elgamal_proof_program/batched_range_proof/mod.rs
+++ zk-sdk/src/zk_elgamal_proof_program/batched_range_proof/mod.rs
@@ -13,7 +13,7 @@ use {
     merlin::Transcript,
     solana_zk_elgamal_proof_interface::proof_data::{BatchedRangeProofContext, MAX_COMMITMENTS},
     solana_zk_sdk_pod::encryption::pedersen::PodPedersenCommitment,
-    std::convert::TryInto,
+    std::{borrow::Borrow, convert::TryInto},
 };
 pub use {batched_range_proof_u128::*, batched_range_proof_u256::*, batched_range_proof_u64::*};
 
@@ -31,12 +31,25 @@ pub(crate) fn batched_range_proof_transcript(context: &BatchedRangeProofContext)
 }
 
 #[allow(non_snake_case)]
-pub(crate) fn build_batched_range_proof_context(
-    commitments: &[&PedersenCommitment],
-    amounts: &[u64],
-    bit_lengths: &[usize],
-    openings: &[&PedersenOpening],
-) -> Result<BatchedRangeProofContext, ProofGenerationError> {
+pub(crate) fn build_batched_range_proof_context<C, PC, A, B, O, PO>(
+    commitments: C,
+    amounts: A,
+    bit_lengths: B,
+    openings: O,
+) -> Result<BatchedRangeProofContext, ProofGenerationError>
+where
+    C: AsRef<[PC]>,
+    PC: Borrow<PedersenCommitment>,
+    A: AsRef<[u64]>,
+    B: AsRef<[usize]>,
+    O: AsRef<[PO]>,
+    PO: Borrow<PedersenOpening>,
+{
+    let commitments = commitments.as_ref();
+    let amounts = amounts.as_ref();
+    let bit_lengths = bit_lengths.as_ref();
+    let openings = openings.as_ref();
+
     // the number of commitments is capped at 8
     let num_commitments = commitments.len();
     if num_commitments > MAX_COMMITMENTS
@@ -49,6 +62,7 @@ pub(crate) fn build_batched_range_proof_context(
 
     let mut pod_commitments = [PodPedersenCommitment::zeroed(); MAX_COMMITMENTS];
     for (i, commitment) in commitments.iter().enumerate() {
+        let commitment = commitment.borrow();
         // all-zero commitment is invalid
         //
         // this check only exists in the prover logic to enforce safe practice
