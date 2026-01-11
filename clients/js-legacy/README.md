e3a784015cfc94276d4554123144301573b7093b
samkim-crypto <skim13@cs.stanford.edu>
2026-01-11 11:39:56 +0900

    remove js-legacy directory

--- clients/js-legacy/README.md
+++
@@ -1,3 +0,0 @@
-# @solana/zk-elgamal-proof
-
-A TypeScript library for interacting with the Solana ZK ElGamal proof program.
--- clients/js-legacy/test/exports/commonjs.cjs
+++
@@ -1,8 +0,0 @@
-const { Keypair } = require('@solana/web3.js');
-const { createCloseContextStateInstruction } = require('../../lib/cjs/index.js');
-
-const contextStateAddress = Keypair.generate().publicKey;
-const destinationAccount = Keypair.generate().publicKey;
-const contextStateAuthority = Keypair.generate().publicKey;
-
-createCloseContextStateInstruction(contextStateAddress, destinationAccount, contextStateAuthority);
