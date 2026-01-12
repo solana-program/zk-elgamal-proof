51e638cb2b86cfacf699c16bff5a1b85ea3ea027
samkim-crypto <skim13@cs.stanford.edu>
2026-01-12 12:15:37 +0900

    clean up `ProofDataError`

--- zk-sdk/src/zk_elgamal_proof_program/proof_data/errors.rs
+++ zk-sdk/src/zk_elgamal_proof_program/proof_data/errors.rs
@@ -2,16 +2,6 @@ use thiserror::Error;
 
 #[derive(Error, Clone, Debug, Eq, PartialEq)]
 pub enum ProofDataError {
-    #[error("decryption error")]
-    Decryption,
-    #[error("missing ciphertext")]
-    MissingCiphertext,
-    #[error("illegal amount bit length")]
-    IllegalAmountBitLength,
-    #[error("arithmetic overflow")]
-    Overflow,
     #[error("invalid proof type")]
     InvalidProofType,
-    #[error("deserialization failed")]
-    Deserialization,
 }
