9b315f5458e64f45a4035ec4255ab17955bff254
samkim-crypto <skim13@cs.stanford.edu>
2026-03-04 12:13:06 +0900

    refactor range proof pod conversion logic

--- zk-sdk/src/range_proof/range_proof.rs
+++ zk-sdk/src/range_proof/range_proof.rs
@@ -5,9 +5,13 @@ use {
             errors::{RangeProofGenerationError, RangeProofVerificationError},
             generators::RangeProofGens,
             inner_product::InnerProductProof,
-            util::self,
+            pod::{PodRangeProofU128, PodRangeProofU256, PodRangeProofU64},
+            util, INNER_PRODUCT_PROOF_U128_LEN, INNER_PRODUCT_PROOF_U256_LEN,
+            INNER_PRODUCT_PROOF_U64_LEN, RANGE_PROOF_MODULO_INNER_PRODUCT_PROOF_LEN,
+            RANGE_PROOF_U128_LEN, RANGE_PROOF_U256_LEN, RANGE_PROOF_U64_LEN,
         },
         transcript::TranscriptProtocol,
+        UNIT_LEN,
     },
     core::iter,
     curve25519_dalek::traits::MultiscalarMul,
@@ -462,6 +466,100 @@ fn delta(bit_lengths: &[usize], y: &Scalar, z: &Scalar) -> Scalar {
         exp_z *= z;
     }
     agg_delta
+}
+
+impl TryFrom<RangeProof> for PodRangeProofU64 {
+    type Error = RangeProofVerificationError;
+
+    fn try_from(decoded_proof: RangeProof) -> Result<Self, Self::Error> {
+        if decoded_proof.ipp_proof.serialized_size() != INNER_PRODUCT_PROOF_U64_LEN {
+            return Err(RangeProofVerificationError::Deserialization);
+        }
+
+        let mut buf = [0_u8; RANGE_PROOF_U64_LEN];
+        copy_range_proof_modulo_inner_product_proof(&decoded_proof, &mut buf);
+        buf[RANGE_PROOF_MODULO_INNER_PRODUCT_PROOF_LEN..RANGE_PROOF_U64_LEN]
+            .copy_from_slice(&decoded_proof.ipp_proof.to_bytes());
+        Ok(PodRangeProofU64(buf))
+    }
+}
+
+impl TryFrom<PodRangeProofU64> for RangeProof {
+    type Error = RangeProofVerificationError;
+
+    fn try_from(pod_proof: PodRangeProofU64) -> Result<Self, Self::Error> {
+        Self::from_bytes(&pod_proof.0)
+    }
+}
+
+#[cfg(not(target_os = "solana"))]
+impl TryFrom<RangeProof> for PodRangeProofU128 {
+    type Error = RangeProofVerificationError;
+
+    fn try_from(decoded_proof: RangeProof) -> Result<Self, Self::Error> {
+        if decoded_proof.ipp_proof.serialized_size() != INNER_PRODUCT_PROOF_U128_LEN {
+            return Err(RangeProofVerificationError::Deserialization);
+        }
+
+        let mut buf = [0_u8; RANGE_PROOF_U128_LEN];
+        copy_range_proof_modulo_inner_product_proof(&decoded_proof, &mut buf);
+        buf[RANGE_PROOF_MODULO_INNER_PRODUCT_PROOF_LEN..RANGE_PROOF_U128_LEN]
+            .copy_from_slice(&decoded_proof.ipp_proof.to_bytes());
+        Ok(PodRangeProofU128(buf))
+    }
+}
+
+#[cfg(not(target_os = "solana"))]
+impl TryFrom<PodRangeProofU128> for RangeProof {
+    type Error = RangeProofVerificationError;
+
+    fn try_from(pod_proof: PodRangeProofU128) -> Result<Self, Self::Error> {
+        Self::from_bytes(&pod_proof.0)
+    }
+}
+
+#[cfg(not(target_os = "solana"))]
+impl TryFrom<RangeProof> for PodRangeProofU256 {
+    type Error = RangeProofVerificationError;
+
+    fn try_from(decoded_proof: RangeProof) -> Result<Self, Self::Error> {
+        if decoded_proof.ipp_proof.serialized_size() != INNER_PRODUCT_PROOF_U256_LEN {
+            return Err(RangeProofVerificationError::Deserialization);
+        }
+
+        let mut buf = [0_u8; RANGE_PROOF_U256_LEN];
+        copy_range_proof_modulo_inner_product_proof(&decoded_proof, &mut buf);
+        buf[RANGE_PROOF_MODULO_INNER_PRODUCT_PROOF_LEN..RANGE_PROOF_U256_LEN]
+            .copy_from_slice(&decoded_proof.ipp_proof.to_bytes());
+        Ok(PodRangeProofU256(buf))
+    }
+}
+
+#[cfg(not(target_os = "solana"))]
+impl TryFrom<PodRangeProofU256> for RangeProof {
+    type Error = RangeProofVerificationError;
+
+    fn try_from(pod_proof: PodRangeProofU256) -> Result<Self, Self::Error> {
+        Self::from_bytes(&pod_proof.0)
+    }
+}
+
+#[cfg(not(target_os = "solana"))]
+fn copy_range_proof_modulo_inner_product_proof(proof: &RangeProof, buf: &mut [u8]) {
+    let mut chunks = buf.chunks_mut(UNIT_LEN);
+    chunks.next().unwrap().copy_from_slice(proof.A.as_bytes());
+    chunks.next().unwrap().copy_from_slice(proof.S.as_bytes());
+    chunks.next().unwrap().copy_from_slice(proof.T_1.as_bytes());
+    chunks.next().unwrap().copy_from_slice(proof.T_2.as_bytes());
+    chunks.next().unwrap().copy_from_slice(proof.t_x.as_bytes());
+    chunks
+        .next()
+        .unwrap()
+        .copy_from_slice(proof.t_x_blinding.as_bytes());
+    chunks
+        .next()
+        .unwrap()
+        .copy_from_slice(proof.e_blinding.as_bytes());
 }
 
 #[cfg(test)]
